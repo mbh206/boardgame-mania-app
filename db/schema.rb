bc4b25ca570ee92aea37f039352a504f3eea5265
christian velez <Csanjo@users.noreply.github.com>
2024-11-19 17:07:18 +0900

    added Thumbnails

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_11_19_055337) do
+ActiveRecord::Schema[7.1].define(version: 2024_11_19_071206) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -51,6 +51,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_11_19_055337) do
     t.datetime "updated_at", null: false
     t.float "complexity"
     t.string "poster"
+    t.string "thumbnail"
   end
 
   create_table "bookings", force: :cascade do |t|
