0864fb309ef4a94ea7a24d2400aa986bacfa5f62
Mark Harris <mark.harris206@gmail.com>
2024-11-19 15:25:54 +0900

    working on offer page still

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -1,2 +1,7 @@
 class OffersController < ApplicationController
+  def show
+    @booking = Booking.find(params[:owner_id])
+    @offer = Offer.find(params[:id])
+    # raise
+  end
 end
--- app/models/offer.rb
+++ app/models/offer.rb
@@ -2,4 +2,5 @@ class Offer < ApplicationRecord
   belongs_to :boardgame
   belongs_to :user
   has_many :bookings, dependent: :destroy
+  has_many_attached :photos
 end
