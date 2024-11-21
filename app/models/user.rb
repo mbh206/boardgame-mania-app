95d4a1f5a812d3baa96f4c4837b7cca55d57f8f8
christian velez <Csanjo@users.noreply.github.com>
2024-11-21 12:06:57 +0900

    added review routes controller model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   has_many :bookings
   has_many :offers
   has_many :bookings_as_owner, through: :offers, source: :bookings
+  has_many :reviews, through: :offers
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
