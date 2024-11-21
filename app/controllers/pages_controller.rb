a87eaba28431df5e6b3a913a740d096a5c892249
Chafique ABIBOURAGUIMANE <shafiq.82@outlook.com>
2024-11-22 00:50:04 +0900

    Better Image preloading w/ Stimulus.js

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,8 @@ class PagesController < ApplicationController
 
   skip_before_action :authenticate_user!, only: :home
   def home
+    
+    @boardgames = Boardgame.all.map { |object| object.poster}
+    puts "hello from home #{@boardgames.count}"
   end
 end
