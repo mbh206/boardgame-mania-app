19a6c37a0e2810bb1d436e94835ae7e38cdb882c
christian velez <Csanjo@users.noreply.github.com>
2024-11-21 23:23:34 +0900

    almost finished search bar

--- app/controllers/boardgames_controller.rb
+++ app/controllers/boardgames_controller.rb
@@ -6,12 +6,12 @@ class BoardgamesController < ApplicationController
   def index  # Steve update to index
      # Get the search query from params
      @query = params[:query]
-
+     @boardgames = Boardgame.order(name: :asc)
      # If there's a query, filter by both name and category
      if @query.present?
        @boardgames = Boardgame.where("name ILIKE ? OR category ILIKE ?", "%#{@query}%", "%#{@query}%")
      else
-       @boardgames = Boardgame.all
+       @boardgames
      end
   end
 
