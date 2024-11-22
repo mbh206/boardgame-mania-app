a7a898a2b48acca5b2d4ebc5861c30b5b8dcf26a
christian velez <Csanjo@users.noreply.github.com>
2024-11-22 12:30:28 +0900

    added css to reviews

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,6 @@
 class Review < ApplicationRecord
   belongs_to :offer
+  belongs_to :user
 
   validates :description, presence: true
   validates :rating, presence: true
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_many :bookings
   has_many :offers
+  has_many :reviews
   has_many :bookings_as_owner, through: :offers, source: :bookings
   has_many :reviews, through: :offers
   # Include default devise modules. Others available are:
